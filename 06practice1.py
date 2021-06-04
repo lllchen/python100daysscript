6363891fba68c803d9e32fded9305bdd188e162b
lllchen <34887405+lllchen@users.noreply.github.com>
2021-06-04 12:46:16 +0800

    solved

--- 06practice1.py
+++ 06practice1.py
@@ -9,6 +9,9 @@
 
 __author__ = 'lllchen'
 
+from functools import reduce
+
+#最大公约数
 def gcd(*numbers):
     least = min(numbers)
     for i in range(1,least+1):
@@ -17,5 +20,18 @@ def gcd(*numbers):
             gcd = i
     return gcd
 
+#最小公倍数
+def lcm(*numbers):
+    largest = max(numbers)
+    product = reduce(lambda x,y:x*y,numbers)
+    for i in range(2,product//largest):
+        lcm = i * largest
+        remainders = list(filter(lambda x: lcm % x == 0,numbers))
+        if len(remainders) == len(numbers):
+            return lcm
+
+
+
 if __name__ == '__main__':
-    print(gcd(6,12,18))
+    print(gcd(6,12,18,25))
+    print(lcm(6,12,18,24))
