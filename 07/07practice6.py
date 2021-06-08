3964df0c077b19139ee9e23b33e120dfeedca884
lllchen <34887405+lllchen@users.noreply.github.com>
2021-06-08 14:25:13 +0800

    solved

--- 07/07practice6.py
+++ 07/07practice6.py
@@ -3,13 +3,25 @@
 
 
 '''
-
 打印杨辉三角
-
 '''
 
 
 __auther__ = 'BrilliantDawn'
 
 def yangTriangle():
-    pass
+    yield [1]
+    l = [1,1]
+    while True:
+        yield l
+        temp =[1]
+        for i in range(len(l)-1):
+            temp.append(l[i]+l[i+1])
+        temp.append(1)
+        l = temp
+
+
+if __name__ == '__main__':
+    yt = yangTriangle()
+    for i in range(10):
+        print(next(yt))
